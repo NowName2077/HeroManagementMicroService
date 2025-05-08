f3c42b4694102e28709cedd87f09ba989f6a9196
NowName2077 <nowname2077@gmail.com>
2025-05-08 19:35:39 +0700

    Ubdate 6

---
+++ Domain/HeroManagement.Domain.Aggregates/Exceptions/ArgumentNullValueException.cs
@@ -0,0 +1,4 @@
+﻿namespace HeroManagement.Domain.Aggregates.Exceptions;
+
+public class ArgumentNullValueException(string paramName)
+    : ArgumentNullException(paramName, $"Argument \"{paramName}\" value is null");
